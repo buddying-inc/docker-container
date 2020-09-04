a6811934f6967414fb8debfb24a5a1d0cc80d68f
Kouhei Sano <apple.x.co@gmail.com>
2020-09-04 12:04:31 +0900

    feat: buddying-centos68-apache-php70

---
+++ buddying-centos68-apache-php70/setup/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+xdebug=${PHP_XDEBUG:-off}
+xdebug=`echo $xdebug | tr '[A-Z]' '[a-z]'`
+
+cd /etc/php.d
+if [ $xdebug != 'on' -a -e 15-xdebug.ini ]; then
+    mv 15-xdebug.ini 15-xdebug.ini.disable
+fi
+
+service postfix start
+/usr/sbin/httpd -DFOREGROUND
