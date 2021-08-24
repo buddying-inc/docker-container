2eb116cc42d602db61f5f77993ddaa1f95d01b5b
Kouhei Sano <apple.x.co@gmail.com>
2021-08-24 18:13:53 +0900

    refactor: centos68-apache-php70 #73

--- centos68-apache-php70/setup/entrypoint.sh
+++ centos68-apache-php70/setup/entrypoint.sh
@@ -3,8 +3,8 @@
 xdebug=${PHP_XDEBUG:-off}
 xdebug=$(echo "$xdebug" | tr "[:upper:]" "[:lower:]")
 
-if [ "$xdebug" != 'on' ] && [ -e /opt/remi/php70/root/etc/php.d/15-xdebug.ini ]; then
-  mv /opt/remi/php70/root/etc/php.d/15-xdebug.ini /opt/remi/php70/root/etc/php.d/15-xdebug.ini.disable
+if [ "$xdebug" != 'on' ] && [ -e /etc/opt/remi/php70/php.d/15-xdebug.ini ]; then
+  mv /etc/opt/remi/php70/php.d/15-xdebug.ini /etc/opt/remi/php70/php.d/15-xdebug.ini.disable
 fi
 
 service postfix start
