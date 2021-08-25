d159fcc457cf11591f3491e8570c58125701c67f
Kouhei Sano <apple.x.co@gmail.com>
2021-08-25 14:34:51 +0900

    refactor: amazonlinux2-* #73

--- amazonlinux2-apache24-php72/setup/entrypoint.sh
+++ amazonlinux2-apache24-php72/setup/entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-#xdebug=${PHP_XDEBUG:-off}
-#xdebug=`echo $xdebug | tr '[A-Z]' '[a-z]'`
-#
-#cd /etc/php.d
-#if [ $xdebug != 'on' -a -e 15-xdebug.ini ]; then
-#    mv 15-xdebug.ini 15-xdebug.ini.disable
-#fi
+set -e
 
 /usr/sbin/postfix start
-/usr/sbin/httpd
-
-while true
-do
-  sleep 10000
-done
+/usr/sbin/httpd -DFOREGROUND
