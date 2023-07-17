92ce3b165ef2de88c2c5ad8017205647c15a4c6a
apple-x-co <apple.x.co@gmail.com>
2023-07-17 16:57:04 +0900

    chore: add Amazon Linux 2023

---
+++ webserver-al2023/apache24-php81/files/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+xdebug=${PHP_XDEBUG:-off}
+xdebug=$(echo "$xdebug" | tr "[:upper:]" "[:lower:]")
+
+if [ "$xdebug" != 'on' ] && [ -e /etc/php.d/15-xdebug.ini ]; then
+  mv /etc/php.d/15-xdebug.ini /etc/php.d/15-xdebug.ini.disable
+fi
+
+/usr/sbin/postfix start
+/usr/sbin/php-fpm
+/usr/sbin/httpd -DFOREGROUND
